ce696f2a2e860ab583c081dba0323d539511c1fa
Guilherme Z <133171632+guilhermezan42@users.noreply.github.com>
2026-08-29 19:30:15 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+  <div>
+    <h1>   Ask, and it shall be given you; seek, and ye shall find; knock, and it shall be opened unto you </h1>
+    <h1> For every one that asketh receiveth; and he that seeketh findeth; and to him that knocketh it shall be opened. </h1>
+  </div> 
+  )
+};
+
+export default Home;
